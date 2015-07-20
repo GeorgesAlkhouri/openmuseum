4717f3e160beb7ed881aa47dcf0afdd97c2750f7
Georges Alkhouri <georges.alkhouri@gmail.com>
2015-07-20 13:13:44 +0200

    [FIX] Compare picture upload

--- includes/dbhelper/DbInserter.php
+++ includes/dbhelper/DbInserter.php
@@ -6,7 +6,7 @@ class DbInserter
 {
     private $log;
     private $dbIdFetcher;
-    
+
     function DbInserter() {
         $this->log = 'DbInserter';
         $this->dbIdFetcher = new DbIdFetcher();
@@ -17,10 +17,10 @@ class DbInserter
     function insertComparisonPicture($db, $picture) {
 
         $sql = "INSERT INTO comparison_pictures (
-                comparison_picture_id, 
+                comparison_picture_id,
                 image, image_sig)
-                VALUES (comparison_pictures_seq.nextval, 
-                ORDSYS.ORDImage.init('FILE', 'IMGDIR02', '$picture->image_name'), 
+                VALUES (comparison_pictures_seq.nextval,
+                ORDSYS.ORDImage.init(),
                 ORDSYS.ORDImageSignature.init())";
 
         $sql .= " returning comparison_picture_id into :comparison_picture_id";
@@ -35,8 +35,8 @@ class DbInserter
 
         /** Load image data **/
             $this->dbImageUploader = new DbImageUploader();
-            $this->dbImageUploader->uploadImageData($db, $picture->image_path.$picture->image_name, $currentPictureId, 'comparison_pictures', 'comparison_picture_id');
-        
+            $this->dbImageUploader->uploadImageData($db, $picture->image_path, $currentPictureId, 'comparison_pictures', 'comparison_picture_id');
+
         /** Create ImageSignature **/
         $sql = "DECLARE imageObj ORDSYS.ORDImage;
                         image_sigObj ORDSYS.ORDImageSignature;
@@ -44,89 +44,89 @@ class DbInserter
                     SELECT image, image_sig INTO imageObj, image_sigObj
                     FROM comparison_pictures WHERE comparison_picture_id = $currentPictureId FOR UPDATE;
                     image_sigObj.generateSignature(imageObj);
-                UPDATE comparison_pictures SET image_sig = image_sigObj 
+                UPDATE comparison_pictures SET image_sig = image_sigObj
                 WHERE comparison_picture_id = $currentPictureId;
                 COMMIT; END;";
 
         echo "$this->log - $sql <br />";
         $stmt = oci_parse($db, $sql);
         oci_execute($stmt, OCI_NO_AUTO_COMMIT);
-        
+
         oci_commit($db);
         return $currentPictureId;
     }
-    
+
     function insertArtistIfDoesNotExists($db, $artist) {
-        
+
         $artist_id = $this->dbIdFetcher->fetchArtistId($db, $artist);
         if (is_null($artist_id)) {
-            $sql = "INSERT INTO artists (artist_id, firstname, lastname, birth_date, death_date) 
+            $sql = "INSERT INTO artists (artist_id, firstname, lastname, birth_date, death_date)
             VALUES (artists_seq.nextval, '$artist->firstname', '$artist->lastname', TO_DATE('$artist->birth_date', 'dd.mm.yyyy'), TO_DATE('$artist->death_date', 'dd.mm.yyyy'))";
             $this->executeSql($db, $sql);
         }
     }
-    
+
     function insertOwnerIfDoesNotExists($db, $owner) {
-        
+
         $owner->id = $this->dbIdFetcher->fetchOwnerId($db, $owner);
         if (is_null($owner->id)) {
-            $sql = "INSERT INTO owners (owner_id, firstname, lastname) 
+            $sql = "INSERT INTO owners (owner_id, firstname, lastname)
             VALUES (owners_seq.nextval, '$owner->firstname', '$owner->lastname')";
             $this->executeSql($db, $sql);
         }
     }
-    
+
     function insertMuseumExhibitsIfNotExists($db, $museum) {
-        
+
         $museum->exhibits_id = $this->dbIdFetcher->fetchMuseumId($db, $museum);
         if (is_null($museum->exhibits_id)) {
-            $sql = "INSERT INTO museums (museum_id, name, adress, website) 
+            $sql = "INSERT INTO museums (museum_id, name, adress, website)
             VALUES (museums_seq.nextval, '$museum->name', '$museum->adress', '$museum->website')";
             $this->executeSql($db, $sql);
         }
     }
-    
+
     function insertMuseumOwnsIfNotExists($db, $museum) {
-        
+
         $museum->owns_id = $this->dbIdFetcher->fetchMuseumId($db, $museum);
         if (is_null($museum->owns_id)) {
-            $sql = "INSERT INTO museums (museum_id, name, adress, website) 
+            $sql = "INSERT INTO museums (museum_id, name, adress, website)
             VALUES (artists_seq.nextval, '$museum->name', '$museum->adress', '$museum->website')";
             $this->executeSql($db, $sql);
         }
     }
-    
+
     function insertKeyWordIfNotExists($db, $keyword_title) {
-        
+
         $keyword_id = $this->dbIdFetcher->fetchKeywordId($db, $keyword_title);
         if (is_null($keyword_id)) {
-            $sql = "INSERT INTO keywords (keyword_id, title) 
+            $sql = "INSERT INTO keywords (keyword_id, title)
             VALUES (keywords_seq.nextval, '$keyword_title')";
             $this->executeSql($db, $sql);
         }
     }
-    
+
     function insertPictureIfNotExists($db, $picture) {
-        
+
         $picture->id = $this->dbIdFetcher->fetchPictureId($db, $picture);
         if (is_null($picture->id)) {
 
             $sql = "INSERT INTO pictures (
-                    picture_id, name, description, 
-                    image, image_sig, 
-                    creation_date, upload_date, 
+                    picture_id, name, description,
+                    image, image_sig,
+                    creation_date, upload_date,
                     artist_fk, artist_safety_level,
-                    museum_ownes_fk, museum_exhibits_fk, 
-                    museum_exhibits_startdate, museum_exhibits_enddate, 
+                    museum_ownes_fk, museum_exhibits_fk,
+                    museum_exhibits_startdate, museum_exhibits_enddate,
                     owner_fk)
-                    VALUES (pictures_seq.nextval, 
-                    '$picture->name', 
-                    '$picture->description', 
-                    ORDSYS.ORDImage.init(), 
+                    VALUES (pictures_seq.nextval,
+                    '$picture->name',
+                    '$picture->description',
+                    ORDSYS.ORDImage.init(),
                     ORDSYS.ORDImageSignature.init(),
-                    TO_DATE('$picture->creation_date', 'dd.mm.yyyy'), 
-                    TO_DATE('$picture->upload_date', 'dd.mm.yyyy'), 
-                    $picture->artist_fk, 
+                    TO_DATE('$picture->creation_date', 'dd.mm.yyyy'),
+                    TO_DATE('$picture->upload_date', 'dd.mm.yyyy'),
+                    $picture->artist_fk,
                     $picture->artist_safety_level,";
 
             /** Add Optional Parameters **/
@@ -139,7 +139,7 @@ class DbInserter
                 $sql .= "NULL, NULL, NULL, ";
             }else{
                 $sql .= "$picture->museum_exhibits_fk ,
-                    TO_DATE('$picture->museum_exhibits_startdate', 'dd.mm.yyyy'), 
+                    TO_DATE('$picture->museum_exhibits_startdate', 'dd.mm.yyyy'),
                     TO_DATE('$picture->museum_exhibits_enddate', 'dd.mm.yyyy'), ";
             }
             if (empty($picture->owner_fk)) {
@@ -170,40 +170,40 @@ class DbInserter
                         SELECT image, image_sig INTO imageObj, image_sigObj
                         FROM pictures WHERE picture_id = $currentPictureId FOR UPDATE;
                         image_sigObj.generateSignature(imageObj);
-                    UPDATE pictures SET image_sig = image_sigObj 
+                    UPDATE pictures SET image_sig = image_sigObj
                     WHERE picture_id = $currentPictureId;
                     COMMIT; END;";
 
             echo "$this->log - $sql <br />";
             $stmt = oci_parse($db, $sql);
             oci_execute($stmt, OCI_NO_AUTO_COMMIT);
-            
+
             oci_commit($db);
         }
     }
-    
+
     function insertPictureCategoriesIfNotExists($db, $picture_id, $category_ids) {
-        
+
         foreach ($category_ids as $category_id => $value) {
-            $sql = "INSERT INTO PICTURES_CATEGORIES (piccat_id, picture_fk, category_fk) 
+            $sql = "INSERT INTO PICTURES_CATEGORIES (piccat_id, picture_fk, category_fk)
             VALUES (pictures_categories_seq.nextval, $picture_id, $value->id)";
             $this->executeSql($db, $sql);
         }
     }
-    
+
     function insertPictureKeywordsIfNotExists($db, $picture_id, $keyword_ids) {
-        
+
         foreach ($keyword_ids as $keyword_id) {
-            $sql = "INSERT INTO PICTURES_KEYWORDS (pickey_id, picture_fk, keyword_fk) 
+            $sql = "INSERT INTO PICTURES_KEYWORDS (pickey_id, picture_fk, keyword_fk)
             VALUES (pictures_keywords_seq.nextval, $picture_id, $keyword_id)";
             $this->executeSql($db, $sql);
         }
     }
-    
+
     /********** Fill Db with Predefined Values ****************/
-    
+
     function insertCategoriesIfNeeded($db) {
-        
+
         $title = 'Bildepoche';
         if (is_null($this->dbIdFetcher->fetchCategoryId($db, $title))) {
             $sql = "INSERT ALL
@@ -215,13 +215,13 @@ class DbInserter
             INTO categories (category_id, title, category_fk) VALUES (6, 'Expressionismus', 3)
             INTO categories (category_id, title, category_fk) VALUES (7, 'Surrealismus', 3)
             SELECT * FROM dual";
-            
+
             $this->executeSql($db, $sql);
         }
     }
-    
+
     function executeSql($db, $sql) {
-        
+
         echo "$this->log - $sql <br />";
         $stmt = oci_parse($db, $sql);
         oci_execute($stmt);
